fd83d91874b29946ca09d9cb6851657d499701d6
Имран <imran@MacBook-Pro-Imran.local>
2023-11-01 17:51:46 +0300

    create listing page and use Swiper to add the image slider

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
-
+import Listing from "./pages/Listing";
 function App() {
   return (
     <>
@@ -25,9 +25,11 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/offers" element={<Offers />} />
-         
           <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
